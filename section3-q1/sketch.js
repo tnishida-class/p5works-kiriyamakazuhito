f280ce6d43e5b8def8058403751526f02750c664
kiriyamakazuhito <73148452+kiriyamakazuhito@users.noreply.github.com>
2020-11-14 17:09:59 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -2,17 +2,19 @@
 // 吹き出しの位置、背景色 etc. を関数 balloon の引数で指定できるようにしてみよう
 // 吹き出しにしっぽを付けてみよう
 function setup(){
-  createCanvas(400, 400);
-  background(255);
-  balloon("I love keyakizaka46");
-}
-
-function balloon(t){
-  let w = textWidth(t);
-  let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
-}
+    createCanvas(400, 400);
+    background(255);
+    balloon(100,150,"I love keyakizaka46",50,150,0);
+  }
+  
+  function balloon(x,y,t,R,G,B){  //引数(左端、上端、テキスト、RGBカラー)
+    let w = textWidth(t);
+    let h = textAscent() + textDescent();
+    let p = 50;
+    fill(R,G,B);
+    rect(x, y, w + p * 2, h + p * 2);
+    rect(x+w+p*2,y+h+p*2-15,40,15)
+    fill(255);
+    text(t, x+p, y+ p);
+  }
+  
